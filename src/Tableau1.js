a197a025af2eeb42eab403d988ab4bb886bc612c
x1301 <92451531+x1301@users.noreply.github.com>
2022-01-25 21:31:40 +0100

    1

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -21,94 +21,101 @@ class Tableau1 extends Phaser.Scene{
         return frames;
     }
 
-    create(){
-        this.foond = this.add.sprite(515,250,'backg');
-        this.foond.setDisplaySize(50,50)
-        this.anims.create({
-            key: 'backgg',
-            frames: this.getFrames('backg',51),
-            frameRate: 28,
-            repeat: -1
-        });
-        this.foond.play('backgg');
+    jeu(){
+        this.start =1
+        this.text.destroy()
+        if (this.start==1) {
+            this.foond = this.add.sprite(515,250,'backg');
+            this.foond.setDisplaySize(50,50)
+            this.anims.create({
+                key: 'backgg',
+                frames: this.getFrames('backg',51),
+                frameRate: 28,
+                repeat: -1
+            });
+            this.foond.play('backgg');
+            this.speedX = 0
+            while(this.speedX===0){
+                this.speedX = 500*Phaser.Math.Between(-1,1)
+            }
+            this.speedY = Phaser.Math.Between(-750, 750)
+            this.maxspeed = 750
 
+            this.balle = this.physics.add.sprite(this.largeur/2, this.hauteur/2, 'circle')
+            this.balle.setDisplaySize(20, 20)
+            this.balle.body.setBounce(1,1);
+            this.balle.body.setAllowGravity(false)
+
+            let particles = this.add.particles('bb');
+            let particle=particles.createEmitter({
+                alpha: { start: 1, end: 0 },
+                scale: { start: 0.25, end: 0.1},
+                frequency: 1,
+                x: this.balle.x,
+                y: this.balle.y,
+                blendMode: 'ADD'
+            });
+            particle.startFollow(this.balle)
+
+            this.haut = this.physics.add.sprite(0, 0, 'blue').setOrigin(0, 0)
+            this.haut.setDisplaySize(this.largeur, 20)
+            this.haut.body.setAllowGravity(false)
+            this.haut.setImmovable(true);
+            this.bas = this.physics.add.sprite(0, 480, 'pink').setOrigin(0, 0)
+            this.bas.setDisplaySize(this.largeur, 20)
+            this.bas.body.setAllowGravity(false)
+            this.bas.setImmovable(true);
+            this.player1 = this.physics.add.sprite(50, 360, 'blue')
+            this.player1.setDisplaySize(20, 100)
+            this.player1.body.setAllowGravity(false)
+
+            let particles1 = this.add.particles('bp');
+            let particle1 = particles1.createEmitter({
+                scale: { start: 0.15, end: 0 },
+                frequency: 1,
+            });
+            particle1.startFollow(this.player1);
+
+            this.player2 = this.physics.add.sprite(920, 360, 'pink')
+            this.player2.setDisplaySize(20, 100)
+            this.player2.body.setAllowGravity(false)
+            this.player1.setImmovable(true)
+            this.player2.setImmovable(true)
+
+            let particles2 = this.add.particles('pp');
+            let particle2 = particles2.createEmitter({
+                scale: { start: 0.15, end: 0 },
+                frequency: 1,
+            });
+            particle2.startFollow(this.player2);
+
+            let me = this;
+            this.physics.add.collider(this.player1, this.balle,function(){
+                me.rebond(me.player1)
+            })
+            this.physics.add.collider(this.player2, this.balle,function(){
+                me.rebond(me.player2)
+            })
+
+            this.physics.add.collider(this.balle, this.bas)
+            this.physics.add.collider(this.balle, this.haut)
+
+            this.balle.setMaxVelocity(this.maxspeed,this.maxspeed)
+
+            this.player1Speed = 0
+            this.player2Speed = 0
+
+            this.joueurGauche = new Joueur('Player 1','joueurGauche')
+            this.joueurDroite = new Joueur('Player 2','joueurDroite')
+            this.balleAucentre()
+        }
+    }
+
+    create(){
         this.hauteur = 500
         this.largeur = 1000
-        this.speedX = 0
-        while(this.speedX===0){
-            this.speedX = 500*Phaser.Math.Between(-1,1)
-        }
-        this.speedY = Phaser.Math.Between(-750, 750)
-        this.maxspeed = 750
-
-        this.balle = this.physics.add.sprite(this.largeur/2, this.hauteur/2, 'circle')
-        this.balle.setDisplaySize(20, 20)
-        this.balle.body.setBounce(1,1);
-        this.balle.body.setAllowGravity(false)
-
-        let particles = this.add.particles('bb');
-        let particle=particles.createEmitter({
-            alpha: { start: 1, end: 0 },
-            scale: { start: 0.25, end: 0.1},
-            frequency: 1,
-            x: this.balle.x,
-            y: this.balle.y,
-            blendMode: 'ADD'
-        });
-        particle.startFollow(this.balle)
-
-        this.haut = this.physics.add.sprite(0, 0, 'blue').setOrigin(0, 0)
-        this.haut.setDisplaySize(this.largeur, 20)
-        this.haut.body.setAllowGravity(false)
-        this.haut.setImmovable(true);
-        this.bas = this.physics.add.sprite(0, 480, 'pink').setOrigin(0, 0)
-        this.bas.setDisplaySize(this.largeur, 20)
-        this.bas.body.setAllowGravity(false)
-        this.bas.setImmovable(true);
-        this.player1 = this.physics.add.sprite(50, 360, 'blue')
-        this.player1.setDisplaySize(20, 100)
-        this.player1.body.setAllowGravity(false)
-
-        let particles1 = this.add.particles('bp');
-        let particle1 = particles1.createEmitter({
-            scale: { start: 0.15, end: 0 },
-            frequency: 1,
-        });
-        particle1.startFollow(this.player1);
-
-        this.player2 = this.physics.add.sprite(920, 360, 'pink')
-        this.player2.setDisplaySize(20, 100)
-        this.player2.body.setAllowGravity(false)
-        this.player1.setImmovable(true)
-        this.player2.setImmovable(true)
-
-        let particles2 = this.add.particles('pp');
-        let particle2 = particles2.createEmitter({
-            scale: { start: 0.15, end: 0 },
-            frequency: 1,
-        });
-        particle2.startFollow(this.player2);
-
-        let me = this;
-        this.physics.add.collider(this.player1, this.balle,function(){
-            me.rebond(me.player1)
-        })
-        this.physics.add.collider(this.player2, this.balle,function(){
-            me.rebond(me.player2)
-        })
-
-        this.physics.add.collider(this.balle, this.bas)
-        this.physics.add.collider(this.balle, this.haut)
-
-        this.balle.setMaxVelocity(this.maxspeed,this.maxspeed)
-
-        this.player1Speed = 0
-        this.player2Speed = 0
-
-        this.joueurGauche = new Joueur('Player 1','joueurGauche')
-        this.joueurDroite = new Joueur('Player 2','joueurDroite')
-
-        this.balleAucentre();
+        this.start = 0
+        this.text=this.add.text(this.largeur/2-300, 350, 'Press Space To Start').setOrigin(0,0).setFontSize(50)
         this.initKeyboard()
     }
 
@@ -125,27 +132,8 @@ class Tableau1 extends Phaser.Scene{
 
     }
 
-    balleAucentre(){
-        this.balle.x = this.largeur/2
-        this.balle.y = this.hauteur/2
-        this.speedX = 0
-
-        this.balle.setVelocityX(Math.random()>0.5?-300:300)
-        this.balle.setVelocityY(0)
-    }
-
-    /**
-     *
-     * @param {Joueur} joueur
-     */
-    win(joueur){
-        //alert('Joueur '+joueur.name+' gagne')
-        joueur.score ++;
-        //alert('Le score est de '+this.joueurGauche.score+' a '+this.joueurDroite.score)
-        this.balleAucentre();
-    }
-
     update(){
+        if(this.start==1){
         if(this.balle.x>this.largeur){
             this.win(this.joueurGauche);
         }
@@ -167,6 +155,28 @@ class Tableau1 extends Phaser.Scene{
         if(this.player2.y>=430){
             this.player2.y=430
         }
+     }
+    }
+
+
+    balleAucentre(){
+        this.balle.x = this.largeur/2
+        this.balle.y = this.hauteur/2
+        this.speedX = 0
+
+        this.balle.setVelocityX(Math.random()>0.5?-300:300)
+        this.balle.setVelocityY(0)
+    }
+
+    /**
+     *
+     * @param {Joueur} joueur
+     */
+    win(joueur){
+        //alert('Joueur '+joueur.name+' gagne')
+        joueur.score ++;
+        //alert('Le score est de '+this.joueurGauche.score+' a '+this.joueurDroite.score)
+        this.balleAucentre();
     }
 
     initKeyboard(){
@@ -201,6 +211,11 @@ class Tableau1 extends Phaser.Scene{
                 case Phaser.Input.Keyboard.KeyCodes.N:
                     me.player2Speed = 0
                     break;
+                case Phaser.Input.Keyboard.KeyCodes.SPACE:
+                    if(me.start==0) {
+                        me.jeu()
+                        break;
+                    }
             }
         });
     }
